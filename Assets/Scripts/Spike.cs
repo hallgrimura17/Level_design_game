67afcee6fb18b42cb634daf06e5613a2da18fa5c
halli <hallgrimura17@ru.is>
2020-02-28 13:12:14 +0000

    5 levels donw

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -5,27 +5,15 @@ namespace AGDDPlatformer
 {
     public class Spike : MonoBehaviour
     {
-        public string playerTag;
-        public bool isSatisfied;
-
-        public GameObject satisfactionIndicator;
-        public AudioSource source;
-
-        void Start()
-        {
-            // satisfactionIndicator.SetActive(false);
-        }
-
         void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player1") || other.CompareTag("Player2"))
             {
-                if (other.GetComponent<Rigidbody2D>().velocity.y < 0){
+                var rb = other.GetComponent<PlayerController>();
+                if (rb.velocity.y > 3 || rb.velocity.y < -3){
                     other.GetComponent<PlayerController>().ResetPlayer();
                 }
 
-                // satisfactionIndicator.SetActive(true);
-                // source.Play();
             }
         }
     }
